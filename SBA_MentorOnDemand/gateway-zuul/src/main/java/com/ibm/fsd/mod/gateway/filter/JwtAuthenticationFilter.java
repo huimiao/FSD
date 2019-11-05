e947fac733b61adb02631e41685e1d2b99b1e041
HUI MIAO <huimiao@cn.ibm.com>
2019-11-05 23:34:18 +0800

    add role authentication

--- SBA_MentorOnDemand/gateway-zuul/src/main/java/com/ibm/fsd/mod/gateway/filter/JwtAuthenticationFilter.java
+++ SBA_MentorOnDemand/gateway-zuul/src/main/java/com/ibm/fsd/mod/gateway/filter/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import com.ibm.fsd.mod.auth.client.AuthClient;
 import com.ibm.fsd.mod.auth.dto.GenericAuthResponse;
 import com.ibm.fsd.mod.auth.dto.TokenUserDto;
 import com.ibm.fsd.mod.common.auth.Sessions;
+import com.ibm.fsd.mod.gateway.config.ModRequestMatcherList;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,9 @@ import javax.servlet.http.HttpServletRequest;
 public class JwtAuthenticationFilter extends ZuulFilter {
     @Autowired
     private AuthClient authClient;
+
+    @Autowired
+    private ModRequestMatcherList modRequestMatcherList;
 
     @Override
     public String filterType() {
@@ -38,12 +42,21 @@ public class JwtAuthenticationFilter extends ZuulFilter {
         HttpServletRequest request = currentContext.getRequest();
         String token = Sessions.getToken(request);
 
+
+        log.info(request.getRequestURI());
+
         if (!StringUtils.isEmpty(token)) {
             GenericAuthResponse genericAuthResponse = authClient.verifyJwtToken(token);
 
             if (genericAuthResponse.isSuccess()) {
                 TokenUserDto tokenUserDto = genericAuthResponse.getTokenUserDto();
                 log.info(tokenUserDto.toString());
+                if(!validAccess(request, tokenUserDto.getRole())){
+                    log.warn("illegal access with correct token");
+                    currentContext.setSendZuulResponse(false);
+                    currentContext.setResponseBody("illegal access");
+                    currentContext.setResponseStatusCode(401);
+                }
                 return null;
             } else {
                 log.warn("Token validation failed");
@@ -51,8 +64,37 @@ public class JwtAuthenticationFilter extends ZuulFilter {
                 currentContext.setResponseBody(genericAuthResponse.getMessage());
                 currentContext.setResponseStatusCode(401);
             }
+        } else {
+            if (!validAccess(request)) {
+                log.warn("illegal access without token");
+                currentContext.setSendZuulResponse(false);
+                currentContext.setResponseBody("illegal access");
+                currentContext.setResponseStatusCode(401);
+            }
         }
 
         return null;
+    }
+
+    private boolean validAccess(HttpServletRequest request, String role) {
+        if (role == null || !(role.equals("ROLE_USER") || role.equals("ROLE_MENTOR"))) {
+            if (modRequestMatcherList.getAll().stream().anyMatch(m -> m.matches(request))) {
+                return false;
+            }
+        }
+
+        if (role.equals("ROLE_USER") && modRequestMatcherList.getMentor().stream().anyMatch(m -> m.matches(request))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validAccess(HttpServletRequest request) {
+        if (modRequestMatcherList.getAll().stream().anyMatch(m -> m.matches(request))) {
+            return false;
+        }
+
+        return true;
     }
 }
